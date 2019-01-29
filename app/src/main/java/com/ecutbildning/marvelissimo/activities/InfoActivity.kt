eeb2d8f6a472ae71e25a6846e1ea68178bd2181f
Mikael Andersson <mickepunktjohn@gmail.com>
2019-01-29 11:49:17 +0100

    Added InfoActivity and activity_info.xml

---
+++ app/src/main/java/com/ecutbildning/marvelissimo/activities/InfoActivity.kt
@@ -0,0 +1,14 @@
+package com.ecutbildning.marvelissimo.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ecutbildning.marvelissimo.R
+
+class InfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+
+}
