3676fb4185b1d026763850c5cfefed79e1478393
mohammed <mohammedalmahfoodh@yahoo.com>
2019-02-04 16:18:05 +0100

    Create CharacterInfoFragment and displaying data on click.

--- app/src/main/java/com/ecutbildning/marvelissimo/activities/MainActivity.kt
+++ app/src/main/java/com/ecutbildning/marvelissimo/activities/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.ecutbildning.marvelissimo.activities
 
-import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -8,34 +7,31 @@ import com.ecutbildning.marvelissimo.R
 import com.ecutbildning.marvelissimo.fragments.CharacterSearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), CharacterSearchFragment.OnFragmentInteractionListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
 
-    override fun onFragmentInteraction(uri: Uri) {
-        //Implement
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, CharacterSearchFragment.newInstance())
+            .commit()
     }
-
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_characters -> {
-                val transaction = supportFragmentManager.beginTransaction()
-                transaction.replace(R.id.container, CharacterSearchFragment.newInstance())
-                transaction.commit()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, CharacterSearchFragment.newInstance())
+                    .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_comics -> {
-                message.setText(R.string.title_comics)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_favourites -> {
-                message.setText(R.string.title_favourites)
                 return@OnNavigationItemSelectedListener true
             }
         }
