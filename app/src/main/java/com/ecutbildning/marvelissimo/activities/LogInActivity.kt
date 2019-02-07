422b090b4d9f64cc9570d8af0d36c8d1202dd8c8
mohammed <mohammedalmahfoodh@yahoo.com>
2019-02-07 18:12:50 +0100

    create log in activity and make it launcher

---
+++ app/src/main/java/com/ecutbildning/marvelissimo/activities/LogInActivity.kt
@@ -0,0 +1,13 @@
+package com.ecutbildning.marvelissimo.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ecutbildning.marvelissimo.R
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
