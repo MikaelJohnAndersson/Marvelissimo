21f275065389fdd3049eda00f1eaf46a5e188f2c
Mikael Andersson <mickepunktjohn@gmail.com>
2019-02-07 10:42:03 +0100

    Populate collapsible with character data and flush git cache

--- app/src/main/java/com/ecutbildning/marvelissimo/fragments/CharacterInfoFragment.kt
+++ app/src/main/java/com/ecutbildning/marvelissimo/fragments/CharacterInfoFragment.kt
@@ -20,7 +20,7 @@ class CharacterInfoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var character: Character? = null
 
-    /*private var expandableListView: ExpandableListView? = null
+    private var expandableListView: ExpandableListView? = null
     private var adapter: ExpandableListAdapter? = null
     private var titleList: List<String>? = null
 
@@ -28,29 +28,13 @@ class CharacterInfoFragment : Fragment() {
         get() {
             val listData = HashMap<String, List<String>>()
 
-            val description = ArrayList<String>()
-            description.add("Whatever description")
-
-            val comics = ArrayList<String>()
-            comics.add("whatever comic")
-
-            val stories = ArrayList<String>()
-            stories.add("whatever stories")
-
-            val events = ArrayList<String>()
-            events.add("whatever events")
-
-            val series = ArrayList<String>()
-            series.add("Whatever serie")
-
-            listData["Description"] = description
-            listData["Comics"] = comics
-            listData["Stories"] = stories
-            listData["Events"] = events
-            listData["Series"] = series
+            listData["Comics"] = ArrayList()
+            listData["Stories"] = ArrayList()
+            listData["Events"] = ArrayList()
+            listData["Series"] = ArrayList()
 
             return listData
-        }*/
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,38 +58,46 @@ class CharacterInfoFragment : Fragment() {
             .fit()
             .into(infoImg)
 
+        expandableListView = rootView.expandableListView
+        val listData = data
+        if (expandableListView != null) {
+           titleList = ArrayList(listData.keys)
+           adapter = ExpansionPanelAdapter(activity as Context, titleList as ArrayList<String>, listData)
+           expandableListView!!.setAdapter(adapter)
+        }
+
             if(!character?.description.isNullOrEmpty()){
                 val description = rootView.description
                 description.text = character?.description
             }
             if (!character?.comics?.items.isNullOrEmpty()){
-                val comics = rootView.comics
                 val comicinfo = character?.comics?.items?.joinToString("\n") { it.name }
-                comics.text = comicinfo
+                val comicData = listData["Comics"] as ArrayList<String>
+                comicData.add(comicinfo!!)
+             /* val comics = rootView.comics
+                comics.text = comicinfo*/
             }
             if(!character?.stories?.items.isNullOrEmpty()){
-                val stories = rootView.stories
                 val storiesinfo = character?.stories?.items?.joinToString("\n") { it.name }
-                stories.text = storiesinfo
+                val storiesData = listData["Stories"] as ArrayList<String>
+                storiesData.add(storiesinfo!!)
+               /* val stories = rootView.stories
+                stories.text = storiesinfo*/
             }
             if(!character?.events?.items.isNullOrEmpty()){
-                val events = rootView.events
                 val eventinfo = character?.events?.items?.joinToString("\n") { it.name }
-                events.text = eventinfo
+                val eventData = listData["Events"] as ArrayList<String>
+                eventData.add(eventinfo!!)
+                /*val events = rootView.events
+                events.text = eventinfo*/
             }
             if(!character?.series?.items.isNullOrEmpty()){
-                val series = rootView.series
                 val seriesinfo = character?.series?.items?.joinToString("\n") { it.name }
-                series.text = seriesinfo
+                val seriesData = listData["Series"] as ArrayList<String>
+                seriesData.add(seriesinfo!!)
+               /* val series = rootView.series
+                series.text = seriesinfo*/
             }
-
-        /*expandableListView = rootView.expandableListView
-        if (expandableListView != null) {
-            val listData = data
-            titleList = ArrayList(listData.keys)
-            adapter = ExpansionPanelAdapter(activity as Context, titleList as ArrayList<String>, listData)
-            expandableListView!!.setAdapter(adapter)
-        }*/
         return rootView
     }
 
