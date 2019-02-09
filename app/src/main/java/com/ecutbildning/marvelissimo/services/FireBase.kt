143b4a077d1c49fd2409404c8389e66e2e594061
mohammed <mohammedalmahfoodh@yahoo.com>
2019-02-09 14:41:20 +0100

    firebase sign in function and login activity added

---
+++ app/src/main/java/com/ecutbildning/marvelissimo/services/FireBase.kt
@@ -0,0 +1,38 @@
+package com.ecutbildning.marvelissimo.services
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.view.View
+import com.ecutbildning.marvelissimo.R
+import com.ecutbildning.marvelissimo.activities.MainActivity
+import com.ecutbildning.marvelissimo.snackBar.SnackBarManager
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+object FireBase {
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private  var user: FirebaseUser? = null
+    private var snackBarManager: SnackBarManager = SnackBarManager()
+
+
+    fun signIn(email: String, password: String, context: Context, view: View) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task: Task<AuthResult> ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    user = auth.currentUser
+                    //   userDataRef = database.child("users").child(user!!.uid)
+                    //updateUI(user)
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                } else {
+                    snackBarManager.createSnackbar(view, "wrong credentials", Color.RED)
+                }
+
+            }
+    }
+
+}
