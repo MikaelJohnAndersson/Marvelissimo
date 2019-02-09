96b1132b9f579088cfc15c70f666a8ddde97d058
mohammed <mohammedalmahfoodh@yahoo.com>
2019-02-09 15:09:21 +0100

    add log in functionality

--- app/src/main/java/com/ecutbildning/marvelissimo/activities/LogInActivity.kt
+++ app/src/main/java/com/ecutbildning/marvelissimo/activities/LogInActivity.kt
@@ -1,14 +1,49 @@
 package com.ecutbildning.marvelissimo.activities
 
+import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ecutbildning.marvelissimo.R
+import com.ecutbildning.marvelissimo.services.FireBase
+import com.ecutbildning.marvelissimo.snackBar.SnackBarManager
+import kotlinx.android.synthetic.main.activity_log_in2.*
 
-class LogInActivity : AppCompatActivity() {
-
+class LogInActivity : AppCompatActivity() , View.OnClickListener {
+    private lateinit var view: View
+    private lateinit var snackbarManager: SnackBarManager
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in2)
+        view = findViewById(android.R.id.content)
+        snackbarManager = SnackBarManager()
+        btnLogIn.setOnClickListener(this)
+        login_register_button.setOnClickListener(this)
+
     }
+
+
+    private fun signIn(email: String, password: String) {
+        if (email.isNotBlank() && password.isNotBlank()) {
+            FireBase.signIn(email, password, this, view)
+
+        } else {
+            snackbarManager.createSnackbar(view, resources.getString(R.string.signin_failed_missing_fields), Color.RED)
+
+        }
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.login_register_button -> {
+                val intent = Intent(this@LogInActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.btnLogIn -> signIn(login_email.text.toString(), login_password.text.toString())
+        }
+    }
+
 }
