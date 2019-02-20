a4b577d3a900e7cc64225324a9790fef83f48b03
Mikael Andersson <mickepunktjohn@gmail.com>
2019-02-20 23:12:45 +0100

    Remove files and functionality related to NavDrawer

--- app/src/main/java/com/ecutbildning/marvelissimo/activities/MainActivity.kt
+++ app/src/main/java/com/ecutbildning/marvelissimo/activities/MainActivity.kt
@@ -3,9 +3,6 @@ package com.ecutbildning.marvelissimo.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import com.ecutbildning.marvelissimo.fragments.CharacterSearchFragment
@@ -14,9 +11,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.support.v7.widget.SearchView
 import android.support.v4.app.Fragment
 import com.ecutbildning.marvelissimo.R
-import android.util.Log
-import android.widget.TextView
-import com.ecutbildning.marvelissimo.dtos.User
 import com.ecutbildning.marvelissimo.fragments.ISearchFragment
 import com.ecutbildning.marvelissimo.services.FireBase
 
@@ -29,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         navigation.setOnNavigationItemSelectedListener(onBottomNavigationItemSelectedListener)
-        nav_view.setNavigationItemSelectedListener(onDrawerNavigationItemSelectedListener)
 
         if(savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -40,25 +33,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu:Menu):Boolean {
         menuInflater.inflate(R.menu.top_navigation, menu)
-
-        val currentUser = FireBase.currentUser
-        findViewById<TextView>(R.id.user_email).text = currentUser?.email
-        findViewById<TextView>(R.id.user_firstname).text = currentUser?.firstName
-        findViewById<TextView>(R.id.user_lastname).text = currentUser?.lastName
-
-        FireBase.loadOnlineUsers { documents ->
-            for (document in documents) {
-                val user = document.toObject(User::class.java)
-                Log.d("MAIN_ACTIVITY", user.firstName + " " + user.lastName)
-            }
-        }
-
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, top_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        drawer_layout.bringToFront()
-        toggle.syncState()
 
         val searchItem = menu.findItem(R.id.navigation_search)
         val searchView = searchItem.actionView as SearchView
@@ -108,24 +82,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         false
-    }
-
-    private val onDrawerNavigationItemSelectedListener = NavigationView.OnNavigationItemSelectedListener { item ->
-
-        when (item.itemId) {
-           R.id.users -> {
-               val userGroupsIsVisible = nav_view.menu.findItem(R.id.nav_online).isVisible
-               nav_view.menu.findItem(R.id.nav_online).isVisible = !userGroupsIsVisible
-               nav_view.menu.findItem(R.id.nav_offline).isVisible = !userGroupsIsVisible
-            }
-            R.id.logout -> {
-                FireBase.signOut()
-                val intent = Intent(this, LogInActivity::class.java)
-                startActivity(intent)
-            }
-            else -> drawer_layout.closeDrawer(GravityCompat.START)
-        }
-        return@OnNavigationItemSelectedListener true
     }
 
     override fun onStop() {
